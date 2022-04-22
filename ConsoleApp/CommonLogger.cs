652e1c2781c902a402a14d235e57ae7b82c295d1
Klaudiusz Kolegowicz <klaudiusz.kolegowicz@microsoft.wsei.edu.pl>
2022-04-22 21:01:52 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp/CommonLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp.Logger;
+using ConsoleApp;
+using System.Net;
+using System.Net.Sockets;
+namespace ConsoleApp.Logger
+{
+    class CommonLogger : ILogger
+    {
+        private ILogger[] loggers;
+        public CommonLogger(ILogger[] loggers)
+        {
+            this.loggers = loggers;
+        }
+        public virtual void Log(params String[] messages)
+        {
+            
+        }
+        public void Dispose(bool disposing)
+        {
+            this.Dispose(disposing: true);
+
+            GC.SuppressFinalize(this);
+        }
+
+
+    }
+}
---
+++ ConsoleApp/ConsoleLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp;
+using ConsoleApp.Logger;
+
+    public  class ConsoleLogger:WriterLogger
+    {
+        public ConsoleLogger()
+    {
+        writer = Console.Out;
+    }
+        public  void consolelog()
+        {
+           
+        }
+
+    public override void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}   
+
